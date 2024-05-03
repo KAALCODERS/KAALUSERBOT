a05514f528c2037ff6db373c747e8a26de30b623
KAALCODERS <85062023+KAALCODERS@users.noreply.github.com>
2024-05-04 00:58:55 +0530

    Update global.py

--- kaal/modules/globals/global.py
+++ kaal/modules/globals/global.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = [1669178360, 1450303652]
+DEVS = [1288699612, 6906107550]
 from kaal.helper.PyroHelpers import get_ub_chats
 from kaal.modules.basic.profile import extract_user, extract_user_and_reason
 from kaal.database import gbandb as kaal
